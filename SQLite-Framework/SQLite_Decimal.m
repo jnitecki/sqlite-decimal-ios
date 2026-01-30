315b9694a93bc06f5363397e5901d5990ab6167f
Jan Nitecki <jnitecki@tbss-ltd.com>
2026-01-30 22:06:08 +0400

    Initial version

---
+++ SQLite-Framework/SQLite_Decimal.m
@@ -0,0 +1,9 @@
+//
+//  SQLite_Decimal.m
+//  This file exists solely to force compilation of the umbrella header
+//
+//  Created by Jan Nitecki on 2026-04-28.
+//
+
+// Forces the linker to run and produce a binary
+__attribute__((visibility("default"))) void SQLiteDecimalFrameworkEntry(void) {}
